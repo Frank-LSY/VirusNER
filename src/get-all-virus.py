93181199f9355ece93d5b45565f249bf9bef41ef
Frank-LSY <429598469@qq.com>
2019-03-04 18:11:29 +0800

    3.4

---
+++ src/get-all-virus.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  4 16:02:27 2019
+
+@author: frank-lsy
+"""
+
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(open('./raw-html.html'))
+#print(soup.prettify())
+f = open('../table/all-virus.txt','w')
+i = 0
+for link in soup.find_all('ul'):
+    if (i<2):
+        i +=1
+        continue
+    i += 1
+    for li in link.find_all('a'):
+        #f.write(li.string+'\n')
+        print(li.string)
+f.close()
+#print(soup.select('.div-col columns column-width')[0].text)
